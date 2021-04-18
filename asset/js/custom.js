8f94159aa980c42bd4354be0c73092a75bceff00
devshisir <contact.shisirimran@gmail.com>
2021-04-18 23:09:30 +0600

    Fix some issue

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -3,5 +3,30 @@ $(document).ready(function() {
     var page = $('html, body');
 
     
+
+    $(window).scroll(function(){
+        var sticky = $('.sticky'),
+            scroll = $(window).scrollTop();
+      
+        if (scroll >= 1) sticky.addClass('animate__animated animate__slideInDown fixed');
+        else sticky.removeClass('animate__animated animate__slideInDown fixed');
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 });
